fa7a57b8e4fbcb59411ae6ea1f4ce9c8508e05b6
Maxim Treskin <zerthurd@gmail.com>
2013-02-21 00:58:02 +0700

    Enhance RPC

--- rpc.go
+++ rpc.go
@@ -4,13 +4,32 @@ import (
 	"github.com/goerlang/etf"
 )
 
+type rpcFunction func(etf.List) etf.Term
+
+type modFun struct {
+	module string
+	function string
+}
+
 type rpcRex struct {
 	GenServerImpl
+	callMap map[modFun]rpcFunction
+}
+
+func (currNode *Node) RpcProvide(modName string, funName string, fun rpcFunction) (err error) {
+	nLog("Provide: %s:%s %#v", modName, funName, fun)
+	currNode.sysProcs.rpcRex.callMap[modFun{modName, funName}] = fun
+	return
+}
+
+func (currNode *Node) RpcRevoke(modName, funName string) {
+	nLog("Revoke: %s:%s", modName, funName)
 }
 
 func (rpcs *rpcRex) Init(args ...interface{}) {
 	nLog("REX: Init: %#v", args)
 	rpcs.Node.Register(etf.Atom("rex"), rpcs.Self)
+	rpcs.callMap = make(map[modFun]rpcFunction, 0)
 }
 
 func (rpcs *rpcRex) HandleCast(message *etf.Term) {
@@ -19,23 +38,28 @@ func (rpcs *rpcRex) HandleCast(message *etf.Term) {
 
 func (rpcs *rpcRex) HandleCall(message *etf.Term, from *etf.Tuple) (reply *etf.Term) {
 	nLog("REX: HandleCall: %#v, From: %#v", *message, *from)
+	var replyTerm etf.Term
+	valid := false
 	switch req := (*message).(type) {
 	case etf.Tuple:
 		if len(req) > 0 {
 			switch act := req[0].(type) {
 			case etf.Atom:
 				if string(act) == "call" {
-					nLog("RPC CALL: Module: %#v, Function: %#v, Args: %#v, GroupLeader: %#v", req[1], req[2], req[3], req[4])
-					replyTerm := etf.Term(etf.Tuple{req[1], req[2]})
-					reply = &replyTerm
+					valid = true
+					if fun, ok := rpcs.callMap[modFun{string(req[1].(etf.Atom)),string(req[2].(etf.Atom))}]; ok {
+						replyTerm = fun(req[3].(etf.List))
+					} else {
+						replyTerm = etf.Term(etf.Tuple{etf.Atom("badrpc"), etf.Tuple{etf.Atom("EXIT"), etf.Tuple{etf.Atom("undef"), etf.List{etf.Tuple{req[1], req[2], req[3], etf.List{} }}}}})
+					}
 				}
 			}
 		}
 	}
-	if reply == nil {
-		replyTerm := etf.Term(etf.Tuple{etf.Atom("badrpc"), etf.Atom("unknown")})
-		reply = &replyTerm
+	if ! valid {
+		replyTerm = etf.Term(etf.Tuple{etf.Atom("badrpc"), etf.Atom("unknown")})
 	}
+	reply = &replyTerm
 	return
 }
 
