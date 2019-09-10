558f497de83f6d867738fc84d4491be3cedcd1ac
Taras Halturin <halturin@gmail.com>
2019-09-10 12:49:10 +0200

    sync

--- gen_server_test.go
+++ gen_server_test.go
@@ -3,78 +3,154 @@ package ergonode
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/halturin/ergonode/etf"
 )
 
+// This test is checking cases below:
+//
+// - starting 2 nodes (node1, node2)
+// - starting 4 GenServers
+//	 * 2 on node1 - gs1, gs2
+// 	 * 2 on node2 - gs3, gs4
+//
+// - local sending
+//  * send: node1 (gs1) -> node1 (gs2). in fashion of erlang sending `erlang:send`
+//  * cast: node1 (gs1) -> node1 (gs2). like `gen_server:cast` does
+//  * call: node1 (gs1) -> node1 (gs2). like `gen_server:call` does
+//
+// - remote sending
+//  * send: node1 (gs1) -> node2 (gs3)
+//  * cast: node1 (gs1) -> node2 (gs3)
+//  * call: node1 (gs1) -> node2 (gs3)
+
 type testGenServer struct {
 	GenServer
 	process Process
+	err     chan error
 }
 
 func (tgs *testGenServer) Init(p Process, args ...interface{}) (state interface{}) {
 	tgs.process = p
+	tgs.err <- nil
 	return nil
 }
 func (tgs *testGenServer) HandleCast(message etf.Term, state interface{}) (string, interface{}) {
-	fmt.Printf("testGenServer ({%s, %s}): HandleCast: %#v\n", tgs.process.name, tgs.process.Node.FullName, message)
+	// fmt.Printf("testGenServer ({%s, %s}): HandleCast: %#v\n", tgs.process.name, tgs.process.Node.FullName, message)
+	tgs.err <- nil
 	return "noreply", state
 }
 func (tgs *testGenServer) HandleCall(from etf.Tuple, message etf.Term, state interface{}) (string, etf.Term, interface{}) {
-	fmt.Printf("testGenServer ({%s, %s}): HandleCall: %#v, From: %#v\n", tgs.process.name, tgs.process.Node.FullName, message, from)
-	reply := etf.Term(etf.Atom("reply"))
-	return "reply", reply, state
+	// fmt.Printf("testGenServer ({%s, %s}): HandleCall: %#v, From: %#v\n", tgs.process.name, tgs.process.Node.FullName, message, from)
+	return "reply", message, state
 }
 func (tgs *testGenServer) HandleInfo(message etf.Term, state interface{}) (string, interface{}) {
-	fmt.Printf("testGenServer ({%s, %s}): HandleInfo: %#v\n", tgs.process.name, tgs.process.Node.FullName, message)
+	// fmt.Printf("testGenServer ({%s, %s}): HandleInfo: %#v\n", tgs.process.name, tgs.process.Node.FullName, message)
+	tgs.err <- nil
 	return "noreply", state
 }
 func (tgs *testGenServer) Terminate(reason string, state interface{}) {
-	fmt.Printf("testGenServer ({%s, %s}): Terminate: %#v\n", tgs.process.name, tgs.process.Node.FullName, reason)
+	// fmt.Printf("testGenServer ({%s, %s}): Terminate: %#v\n", tgs.process.name, tgs.process.Node.FullName, reason)
+	tgs.err <- nil
 }
 
 func TestGenServer(t *testing.T) {
+	fmt.Printf("Starting nodes: node1@localhost, node1@localhost: ")
 	node1 := CreateNode("node1@localhost", "cookies", NodeOptions{})
 	node2 := CreateNode("node2@localhost", "cookies", NodeOptions{})
-
-	gs1 := &testGenServer{}
-	gs2 := &testGenServer{}
-	gs3 := &testGenServer{}
-	gs4 := &testGenServer{}
-
-	node1p1, _ := node1.Spawn("gs1", ProcessOptions{}, gs1, nil)
-	node1p2, _ := node1.Spawn("gs2", ProcessOptions{}, gs2, nil)
-	node2p1, _ := node2.Spawn("gs3", ProcessOptions{}, gs3, nil)
-	node2p2, _ := node2.Spawn("gs4", ProcessOptions{}, gs4, nil)
-
-	fmt.Println("Testing GenServer process:")
-	node1p1.Send(node1p2.Self(), etf.Atom("hi"))
-	fmt.Println("    process.Send local -> local: ok")
-
-	node1p1.Cast(node1p2.Self(), etf.Atom("hi cast"))
-	fmt.Println("    process.Cast local -> local : ok")
-
-	if _, err := node1p1.Call(node1p2.Self(), etf.Atom("hi call")); err != nil {
-		fmt.Println("    process.Call local -> local: failed")
-		t.Fatal(err)
+	if node1 == nil || node2 == nil {
+		t.Fatal("can't start nodes")
 	} else {
-		fmt.Println("    process.Call local -> local: ok")
+		fmt.Println("OK")
 	}
 
-	node1p1.Call(node2p1.Self(), etf.Atom("hi remote"))
-	node2p2.Call(node1p1.Self(), etf.Atom("remote hi"))
-	// fmt.Println("    process.Send local -> remote: ok")
+	gs1 := &testGenServer{
+		err: make(chan error, 2),
+	}
+	gs2 := &testGenServer{
+		err: make(chan error, 2),
+	}
+	gs3 := &testGenServer{
+		err: make(chan error, 2),
+	}
 
-	// node1p1.Cast(node2p2.Self(), etf.Atom("hi cast"))
-	// fmt.Println("    process.Cast local -> remote : ok")
+	fmt.Printf("    wait for start of gs1 on %#v: ", node1.FullName)
+	node1gs1, _ := node1.Spawn("gs1", ProcessOptions{}, gs1, nil)
+	waitForResult(t, gs1.err)
 
-	// if _, err := node1p1.Call(node2p2.Self(), etf.Atom("hi call")); err != nil {
-	// 	fmt.Println("    process.Call local -> remote: failed")
-	// 	t.Fatal(err)
-	// } else {
-	// 	fmt.Println("    process.Call local -> remote: ok")
-	// }
+	fmt.Printf("    wait for start of gs2 on %#v: ", node1.FullName)
+	node1gs2, _ := node1.Spawn("gs2", ProcessOptions{}, gs2, nil)
+	waitForResult(t, gs2.err)
 
+	fmt.Printf("    wait for start of gs3 on %#v: ", node2.FullName)
+	node2gs3, _ := node2.Spawn("gs3", ProcessOptions{}, gs3, nil)
+	waitForResult(t, gs3.err)
+
+	fmt.Println("Testing GenServer process:")
+
+	fmt.Printf("    process.Send local (gs1) -> local (gs2) : ")
+	node1gs1.Send(node1gs2.Self(), etf.Atom("hi"))
+	waitForResult(t, gs2.err)
+
+	node1gs1.Cast(node1gs2.Self(), etf.Atom("hi cast"))
+	fmt.Printf("    process.Cast local (gs1) -> local (gs2) : ")
+	waitForResult(t, gs2.err)
+
+	fmt.Printf("    process.Call local (gs1) -> local (gs2): ")
+	v := etf.Atom("hi call")
+	if v1, err := node1gs1.Call(node1gs2.Self(), v); err != nil {
+		t.Fatal(err)
+	} else {
+		if v == v1 {
+			fmt.Println("OK")
+		} else {
+			e := fmt.Errorf("expected: %#v , got: %#v", v, v1)
+			t.Fatal(e)
+		}
+	}
+
+	fmt.Printf("    process.Send local (gs1) -> remote (gs3) : ")
+	node1gs1.Send(node2gs3.Self(), etf.Atom("hi"))
+	waitForResult(t, gs3.err)
+
+	node1gs1.Cast(node2gs3.Self(), etf.Atom("hi cast"))
+	fmt.Printf("    process.Cast local (gs1) -> remote (gs3) : ")
+	waitForResult(t, gs3.err)
+
+	fmt.Printf("    process.Call local (gs1) -> remote (gs3): ")
+	if v1, err := node1gs1.Call(node2gs3.Self(), v); err != nil {
+		t.Fatal(err)
+	} else {
+		if v == v1 {
+			fmt.Println("OK")
+		} else {
+			e := fmt.Errorf("expected: %#v , got: %#v", v, v1)
+			t.Fatal(e)
+		}
+	}
+
+	fmt.Printf("Stopping nodes: %v, %v\n", node1.FullName, node2.FullName)
 	node1.Stop()
 	node2.Stop()
+
+	fmt.Printf("    waiting for termination of gs1: ")
+	waitForResult(t, gs1.err)
+	fmt.Printf("    waiting for termination of gs2: ")
+	waitForResult(t, gs2.err)
+	fmt.Printf("    waiting for termination of gs3: ")
+	waitForResult(t, gs3.err)
+
+}
+
+func waitForResult(t *testing.T, w chan error) {
+	select {
+	case e := <-w:
+		if e == nil {
+			fmt.Println("OK")
+		}
+
+	case <-time.After(time.Second * time.Duration(1)):
+		t.Fatal("result timeout")
+	}
 }
