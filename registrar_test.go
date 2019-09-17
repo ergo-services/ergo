5944dc9cf54ff8b8c9b07c822b17bc608208d408
Taras Halturin <halturin@gmail.com>
2019-09-17 14:17:42 +0200

    sync

--- registrar_test.go
+++ registrar_test.go
@@ -1,23 +1,9 @@
-//+build registrar
-
 package ergonode
 
 import (
 	"testing"
-	"time"
 )
 
-
 func TestCreateRegistrar(t *testing.T) {
-	node := CreateNode("node@localhost","cookies", NodeOptions{})
-
-	g:=&GenServer{}
-	cr := createRegistrar(node)
-	p := cr.RegisterProcess(g)
-	g.Process = p
-	t.Logf("registered processes: %#v \n", cr.Registered())
-	time.Sleep(1 *time.Second)
-	t.Logf("processes: %#v \n", p.self)
-	p.Stop()
-
+	// node := CreateNode("node@localhost","cookies", NodeOptions{})
 }
