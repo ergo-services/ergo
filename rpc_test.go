3d8f7793e48bac0fc3e9d8c043bba9d552a57213
Taras Halturin <halturin@gmail.com>
2019-09-18 13:17:58 +0200

    sync

--- rpc_test.go
+++ rpc_test.go
@@ -1,16 +1,5 @@
 package ergonode
 
-// start node1 with RPC
-//  start n1gs1
-
-//  start node2 with disabled RPC
-//  start n2gs1
-
-//  register func1 as node1.func1 method
-
-//  rpc call n2gs1.call(node1.func1) (receive reply)
-//  rpc call n1gs1.call(node2.whatevername) (recekve badrpc error)
-
 import (
 	"fmt"
 	"reflect"
