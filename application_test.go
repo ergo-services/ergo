b4d95e780c22993c82204346baedcc8ec05fccf9
Taras Halturin <halturin@gmail.com>
2020-02-28 13:51:45 +0100

    sync

--- application_test.go
+++ application_test.go
@@ -251,7 +251,7 @@ func TestApplicationTypePermanent(t *testing.T) {
 		t.Fatal("timeout on waiting child")
 	}
 
-	if e := node.WaitWithTimeout(200 * time.Millisecond); e != nil {
+	if e := node.WaitWithTimeout(1 * time.Second); e != nil {
 		t.Fatal("node shouldn't be alive here")
 	}
 
