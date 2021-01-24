823183f1f292629d56d45bfc5167aa90aa3a4df5
Taras Halturin <halturin@gmail.com>
2021-01-24 09:47:49 +0100

    Fixup

--- windows.go
+++ windows.go
@@ -2,6 +2,8 @@
 
 package ergo
 
-func A() {
-	return
+func os_dep_getResourceUsage() (int64, int64) {
+	// FIXME Windows doesn't support syscall.Rusage. There is should be another
+	// way to get this kind of data from the OS
+	return 0, 0
 }
