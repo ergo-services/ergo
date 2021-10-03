8fc8ba795f3eceb15d8e8c2f1e8fecabb2f1791e
Taras Halturin <halturin@gmail.com>
2021-10-03 21:27:32 +0200

    sync

--- tests/saga_cancel_test.go
+++ tests/saga_cancel_test.go
@@ -85,3 +85,82 @@ func TestSagaCancelSimple(t *testing.T) {
 	fmt.Println("OK", saga_process.Self())
 
 }
+
+//
+// Case 2.a
+//    Saga1 -> Tx -> Saga2 -> Tx -> Saga3
+//       Saga1 cancels Tx
+//       Saga1 -> cancel -> Saga2 -> cancel -> Saga3
+//
+
+func TestSagaCancelCase2a(t *testing.T) {
+	fmt.Printf("\n=== Test GenSagaCancelCase2a\n")
+	fmt.Printf("Starting node: nodeGenSagaCancelCase2a01@localhost...")
+
+	node, _ := ergo.StartNode("nodeGenSagaCancelCase2a01@localhost", "cookies", node.Options{})
+
+	if node == nil {
+		t.Fatal("can't start node")
+		return
+	}
+	fmt.Println("OK")
+}
+
+//
+// Case 2.b
+//    Saga1 -> Tx -> Saga2 -> Tx -> Saga3
+//       Saga2 cancels Tx
+//       Saga1 <- cancel <- Saga2 -> cancel -> Saga3
+//
+func TestSagaCancelCase2b(t *testing.T) {
+	fmt.Printf("\n=== Test GenSagaCancelCase2b\n")
+	fmt.Printf("Starting node: nodeGenSagaCancelCase2b01@localhost...")
+
+	node, _ := ergo.StartNode("nodeGenSagaCancelCase2b01@localhost", "cookies", node.Options{})
+
+	if node == nil {
+		t.Fatal("can't start node")
+		return
+	}
+	fmt.Println("OK")
+}
+
+//
+// Case 2.c
+//    Saga1 -> Tx -> Saga2 -> Tx -> Saga3
+//       Saga3 cancels Tx
+//       Saga1 <- cancel <- Saga2 <- cancel <- Saga3
+//
+func TestSagaCancelCase2c(t *testing.T) {
+	fmt.Printf("\n=== Test GenSagaCancelCase2c\n")
+	fmt.Printf("Starting node: nodeGenSagaCancelCase2c01@localhost...")
+
+	node, _ := ergo.StartNode("nodeGenSagaCancelCase2c01@localhost", "cookies", node.Options{})
+
+	if node == nil {
+		t.Fatal("can't start node")
+		return
+	}
+	fmt.Println("OK")
+}
+
+//
+// Case 2.d
+//    Saga1 -> Tx -> Saga2 -> Tx -> Saga3
+//       Saga1 sets TrapCancel, Saga2 process/node is going down, Saga1 sends Tx to the Saga4
+//              -> Tx -> Saga4 -> Tx -> Saga3
+//            /
+//       Saga1 <- signal Down <- Saga2 (terminates) -> signal Down -> Saga3
+//
+func TestSagaCancelCase2d(t *testing.T) {
+	fmt.Printf("\n=== Test GenSagaCancelCase2d\n")
+	fmt.Printf("Starting node: nodeGenSagaCancelCase2d01@localhost...")
+
+	node, _ := ergo.StartNode("nodeGenSagaCancelCase2d01@localhost", "cookies", node.Options{})
+
+	if node == nil {
+		t.Fatal("can't start node")
+		return
+	}
+	fmt.Println("OK")
+}
