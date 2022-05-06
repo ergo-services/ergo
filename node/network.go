9b625a4227fbead4b3f88a281dee0e46acda3f7e
Taras Halturin <halturin@gmail.com>
2022-05-06 16:56:06 +0200

    sync

--- node/network.go
+++ node/network.go
@@ -1428,8 +1428,10 @@ func generateSelfSignedCert(version Version) (tls.Certificate, error) {
 	template := x509.Certificate{
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
+			CommonName: "localhost",
+		},
+		Subject: pkix.Name{
 			Organization: []string{org},
-			CommonName:   "localhost",
 		},
 		NotBefore: time.Now(),
 		NotAfter:  time.Now().Add(time.Hour * 24 * 365),
