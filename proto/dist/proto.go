a98e83493f99f4682ce93e19a5e3674d43e02936
leon <leonlee2013@gmail.com>
2022-12-03 21:27:15 +0800

    fixbug#116 rpc:call from [Erlang/OTP24] to [Ergo/Go 1.19] is not working

--- proto/dist/proto.go
+++ proto/dist/proto.go
@@ -1297,12 +1297,11 @@ func (dc *distConnection) handleMessage(message *distMessage) (err error) {
 				registerName := ""
 				for _, option := range t.Element(6).(etf.List) {
 					name, ok := option.(etf.Tuple)
-					if !ok || len(name) != 2 {
-						return fmt.Errorf("malformed spawn request")
-					}
-					switch name.Element(1) {
-					case etf.Atom("name"):
-						registerName = string(name.Element(2).(etf.Atom))
+					if ok || len(name) == 2 {
+						switch name.Element(1) {
+						case etf.Atom("name"):
+							registerName = string(name.Element(2).(etf.Atom))
+						}
 					}
 				}
 
@@ -1321,6 +1320,10 @@ func (dc *distConnection) handleMessage(message *distMessage) (err error) {
 					for i := range []byte(str) {
 						args = append(args, str[i])
 					}
+				}
+
+				if registerName == "" && module == etf.Atom("erpc") {
+					registerName = "erpc"
 				}
 
 				spawnRequestOptions := gen.RemoteSpawnOptions{
