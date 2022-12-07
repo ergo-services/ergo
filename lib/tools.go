bfc45ac536a362de120d6bb69c37704cbab5e3ff
leon <leonlee2013@gmail.com>
2022-12-07 20:23:34 +0800

    yyyy

--- lib/tools.go
+++ lib/tools.go
@@ -55,6 +55,7 @@ func init() {
 	// flag.BoolVar(&ergoTrace, "ergo.trace", false, "enable/disable extended debug info")
 	flag.BoolVar(&ergoTrace, "ergo.trace", false, "enable/disable extended debug info")
 	flag.BoolVar(&ergoWarning, "ergo.warning", true, "enable/disable warning messages")
+	// flag.BoolVar(&ergoNoRecover, "ergo.norecover", false, "disable panic catching")
 	flag.BoolVar(&ergoNoRecover, "ergo.norecover", false, "disable panic catching")
 	flag.BoolVar(&ergoDebug, "ergo.debug", false, "enable/disable debug messages")
 }
