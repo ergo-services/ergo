27f3fb04739fa55a78c0793f10f0d0b8da235877
Taras Halturin <halturin@gmail.com>
2025-06-10 10:44:46 +0200

    add more benchs in 'net/edf'

--- net/edf/benchmarks_test.go
+++ net/edf/benchmarks_test.go
@@ -3,13 +3,13 @@ package edf
 import (
 	"bytes"
 	"encoding/gob"
-	"reflect"
 	"sync"
 	"testing"
 
-	"ergo.services/ergo/gen"
 	"ergo.services/ergo/lib"
 )
+
+// ENCODING BENCHMARKS WITHOUT TYPE CACHING
 
 func BenchmarkEncodeString(b *testing.B) {
 	buf := lib.TakeBuffer()
@@ -17,139 +17,8 @@ func BenchmarkEncodeString(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		buf.Reset()
 		if err := Encode("Ergo Framework", buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-func BenchmarkEncodeStringGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-	gob.Register(benchEncodeStruct{})
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode("Ergo Framework"); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeAtom(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if err := Encode(gen.Atom("Ergo Framework"), buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeAtomGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-	gob.Register(benchEncodeStruct{})
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode(gen.Atom("Ergo Framework")); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeBinary(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-	value := []byte{1, 2, 3, 4, 5}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeBinaryGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-
-	value := []byte{1, 2, 3, 4, 5}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode(value); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeSlice(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := []int{12345, 67890, 12345, 67890}
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeSliceGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-
-	value := []int{12345, 67890, 12345, 67890}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode(value); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeArray(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := [4]int{12345, 67890, 12345, 67890}
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeArrayGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-
-	value := [4]int{12345, 67890, 12345, 67890}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode(value); err != nil {
 			b.Fatal(err)
 		}
 	}
@@ -173,9 +42,148 @@ func BenchmarkEncodeMap(b *testing.B) {
 	}
 }
 
+func BenchmarkEncodeSlice(b *testing.B) {
+	buf := lib.TakeBuffer()
+	defer lib.ReleaseBuffer(buf)
+
+	value := []int64{12345, 67890, 12345, 67890}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		if err := Encode(value, buf, Options{}); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// ENCODING BENCHMARKS WITH TYPE CACHING
+
+func BenchmarkEncodeStringCached(b *testing.B) {
+	buf := lib.TakeBuffer()
+	defer lib.ReleaseBuffer(buf)
+
+	cache := &sync.Map{}
+	options := Options{Cache: cache}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		if err := Encode("Ergo Framework", buf, options); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkEncodeMapCached(b *testing.B) {
+	buf := lib.TakeBuffer()
+	defer lib.ReleaseBuffer(buf)
+
+	value := map[string]int{
+		"key1": 1234,
+		"key2": 5678,
+	}
+
+	cache := &sync.Map{}
+	options := Options{Cache: cache}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		if err := Encode(value, buf, options); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkEncodeSliceCached(b *testing.B) {
+	buf := lib.TakeBuffer()
+	defer lib.ReleaseBuffer(buf)
+
+	value := []int64{12345, 67890, 12345, 67890}
+
+	cache := &sync.Map{}
+	options := Options{Cache: cache}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		if err := Encode(value, buf, options); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// COMPLEX TYPES TO SHOW BIGGER IMPACT
+
+func BenchmarkEncodeComplexMap(b *testing.B) {
+	buf := lib.TakeBuffer()
+	defer lib.ReleaseBuffer(buf)
+
+	value := map[string]map[int][]string{
+		"group1": {
+			1: {"item1", "item2", "item3"},
+			2: {"item4", "item5"},
+		},
+		"group2": {
+			3: {"item6", "item7", "item8", "item9"},
+			4: {"item10"},
+		},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		if err := Encode(value, buf, Options{}); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkEncodeComplexMapCached(b *testing.B) {
+	buf := lib.TakeBuffer()
+	defer lib.ReleaseBuffer(buf)
+
+	value := map[string]map[int][]string{
+		"group1": {
+			1: {"item1", "item2", "item3"},
+			2: {"item4", "item5"},
+		},
+		"group2": {
+			3: {"item6", "item7", "item8", "item9"},
+			4: {"item10"},
+		},
+	}
+
+	cache := &sync.Map{}
+	options := Options{Cache: cache}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		if err := Encode(value, buf, options); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// GOB COMPARISONS
+
+func BenchmarkEncodeStringGob(b *testing.B) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		if err := enc.Encode("Ergo Framework"); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
 func BenchmarkEncodeMapGob(b *testing.B) {
 	var buf bytes.Buffer
-
 	enc := gob.NewEncoder(&buf)
 
 	value := map[string]int{
@@ -192,27 +200,11 @@ func BenchmarkEncodeMapGob(b *testing.B) {
 	}
 }
 
-func BenchmarkEncodePID(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := gen.PID{Node: "demo@127.0.0.1", ID: 312, Creation: 2}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodePIDGob(b *testing.B) {
+func BenchmarkEncodeSliceGob(b *testing.B) {
 	var buf bytes.Buffer
-
 	enc := gob.NewEncoder(&buf)
 
-	value := gen.PID{Node: "demo@127.0.0.1", ID: 312, Creation: 2}
+	value := []int64{12345, 67890, 12345, 67890}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -223,175 +215,18 @@ func BenchmarkEncodePIDGob(b *testing.B) {
 	}
 }
 
-func BenchmarkEncodeProcessID(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := gen.ProcessID{Node: "demo@127.0.0.1", Name: "example"}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeProcessIDGob(b *testing.B) {
+func BenchmarkEncodeComplexMapGob(b *testing.B) {
 	var buf bytes.Buffer
-
 	enc := gob.NewEncoder(&buf)
 
-	value := gen.ProcessID{Node: "demo@127.0.0.1", Name: "example"}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode(value); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeRef(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := gen.Ref{
-		Node:     "demo@127.0.0.1",
-		Creation: 2,
-		ID:       [3]uint64{73444, 3082813441, 2373634851},
-	}
-
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeRefGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-
-	value := gen.Ref{
-		Node:     "demo@127.0.0.1",
-		Creation: 2,
-		ID:       [3]uint64{73444, 3082813441, 2373634851},
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode(value); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-type benchEncodeStruct struct {
-	A float32
-	B float64
-	C any
-}
-
-func BenchmarkEncodeStructEDF(b *testing.B) {
-	var regCache *sync.Map
-
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := benchEncodeStruct{
-		A: 3.14,
-		B: 3.15,
-		C: float64(3.18),
-	}
-
-	RegisterTypeOf(benchEncodeStruct{})
-	regCache = new(sync.Map)
-	regCache.Store(reflect.TypeOf(value), []byte{edtReg, 0x13, 0x88})
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{RegCache: regCache}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeStructGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-	gob.Register(benchEncodeStruct{})
-
-	value := benchEncodeStruct{
-		A: 3.14,
-		B: 3.15,
-		C: float64(3.18),
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := enc.Encode(value); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeSliceStructEDF(b *testing.B) {
-	var regCache *sync.Map
-
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := []benchEncodeStruct{
-		{
-			A: 3.14,
-			B: 3.15,
-			C: float64(3.18),
+	value := map[string]map[int][]string{
+		"group1": {
+			1: {"item1", "item2", "item3"},
+			2: {"item4", "item5"},
 		},
-		{
-			A: 3.15,
-			B: 3.14,
-			C: float64(3.19),
-		},
-	}
-
-	RegisterTypeOf(benchEncodeStruct{})
-	regCache = new(sync.Map)
-	regCache.Store(reflect.TypeOf(value), []byte{edtReg, 0x13, 0x88})
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		buf.Reset()
-		if err := Encode(value, buf, Options{RegCache: regCache}); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkEncodeSliceStructGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-	gob.Register(benchEncodeStruct{})
-
-	value := []benchEncodeStruct{
-		{
-			A: 3.14,
-			B: 3.15,
-			C: float64(3.18),
-		},
-		{
-			A: 3.15,
-			B: 3.14,
-			C: float64(3.19),
+		"group2": {
+			3: {"item6", "item7", "item8", "item9"},
+			4: {"item10"},
 		},
 	}
 
@@ -404,83 +239,12 @@ func BenchmarkEncodeSliceStructGob(b *testing.B) {
 	}
 }
 
-// decoding
+// DECODING BENCHMARKS WITHOUT TYPE CACHING
 
 func BenchmarkDecodeString(b *testing.B) {
 	buf := lib.TakeBuffer()
 	defer lib.ReleaseBuffer(buf)
 	if err := Encode("Ergo Framework", buf, Options{}); err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkDecodeAtom(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	if err := Encode(gen.Atom("Ergo Framework"), buf, Options{}); err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkDecodeBinary(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := []byte{1, 2, 3, 4, 5}
-	if err := Encode(value, buf, Options{}); err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkDecodeSlice(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := []int{12345, 67890, 12345, 67890}
-	if err := Encode(value, buf, Options{}); err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkDecodeArray(b *testing.B) {
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := [4]int{12345, 67890, 12345, 67890}
-	if err := Encode(value, buf, Options{}); err != nil {
 		b.Fatal(err)
 	}
 
@@ -514,11 +278,11 @@ func BenchmarkDecodeMap(b *testing.B) {
 	}
 }
 
-func BenchmarkDecodePID(b *testing.B) {
+func BenchmarkDecodeSlice(b *testing.B) {
 	buf := lib.TakeBuffer()
 	defer lib.ReleaseBuffer(buf)
 
-	value := gen.PID{Node: "demo@127.0.0.1", ID: 312, Creation: 2}
+	value := []int{12345, 67890, 12345, 67890}
 	if err := Encode(value, buf, Options{}); err != nil {
 		b.Fatal(err)
 	}
@@ -532,162 +296,70 @@ func BenchmarkDecodePID(b *testing.B) {
 	}
 }
 
-func BenchmarkDecodeProcessID(b *testing.B) {
+// DECODING BENCHMARKS WITH TYPE CACHING
+
+func BenchmarkDecodeStringCached(b *testing.B) {
 	buf := lib.TakeBuffer()
 	defer lib.ReleaseBuffer(buf)
 
-	value := gen.ProcessID{Node: "demo@127.0.0.1", Name: "example"}
-	if err := Encode(value, buf, Options{}); err != nil {
+	cache := &sync.Map{}
+	options := Options{Cache: cache}
+
+	if err := Encode("Ergo Framework", buf, options); err != nil {
 		b.Fatal(err)
 	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
+		_, _, err := Decode(buf.B, options)
 		if err != nil {
 			b.Fatal(err)
 		}
 	}
 }
 
-func BenchmarkDecodeRef(b *testing.B) {
+func BenchmarkDecodeMapCached(b *testing.B) {
 	buf := lib.TakeBuffer()
 	defer lib.ReleaseBuffer(buf)
 
-	value := gen.Ref{
-		Node:     "demo@127.0.0.1",
-		Creation: 2,
-		ID:       [3]uint64{73444, 3082813441, 2373634851},
+	value := map[string]int{
+		"key1": 1234,
+		"key2": 5678,
 	}
-	if err := Encode(value, buf, Options{}); err != nil {
+
+	cache := &sync.Map{}
+	options := Options{Cache: cache}
+
+	if err := Encode(value, buf, options); err != nil {
 		b.Fatal(err)
 	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
+		_, _, err := Decode(buf.B, options)
 		if err != nil {
 			b.Fatal(err)
 		}
 	}
 }
 
-func BenchmarkDecodeStructEDF(b *testing.B) {
-	var regCache *sync.Map
-
+func BenchmarkDecodeSliceCached(b *testing.B) {
 	buf := lib.TakeBuffer()
 	defer lib.ReleaseBuffer(buf)
 
-	value := benchEncodeStruct{
-		A: 3.14,
-		B: 3.15,
-		C: float64(3.18),
-	}
+	value := []int{12345, 67890, 12345, 67890}
 
-	RegisterTypeOf(benchEncodeStruct{})
-	//regCache = new(sync.Map)
-	//regCache.Store(reflect.TypeOf(value), []byte{edtReg, 0x13, 0x88})
-	if err := Encode(value, buf, Options{RegCache: regCache}); err != nil {
+	cache := &sync.Map{}
+	options := Options{Cache: cache}
+
+	if err := Encode(value, buf, options); err != nil {
 		b.Fatal(err)
 	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
+		_, _, err := Decode(buf.B, options)
 		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkDecodeStructGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-	gob.Register(benchEncodeStruct{})
-
-	value := benchEncodeStruct{
-		A: 3.14,
-		B: 3.15,
-		C: float64(3.18),
-	}
-	if err := enc.Encode(value); err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		var x benchEncodeStruct
-		decbuf := bytes.NewBuffer(buf.Bytes())
-		dec := gob.NewDecoder(decbuf)
-		if err := dec.Decode(&x); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkDecodeSliceStructEDF(b *testing.B) {
-	var regCache *sync.Map
-
-	buf := lib.TakeBuffer()
-	defer lib.ReleaseBuffer(buf)
-
-	value := []benchEncodeStruct{
-		{
-			A: 3.14,
-			B: 3.15,
-			C: float64(3.18),
-		},
-		{
-			A: 3.15,
-			B: 3.14,
-			C: float64(3.19),
-		},
-	}
-
-	RegisterTypeOf(benchEncodeStruct{})
-	//regCache = new(sync.Map)
-	//regCache.Store(reflect.TypeOf(value), []byte{edtReg, 0x13, 0x88})
-	if err := Encode(value, buf, Options{RegCache: regCache}); err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _, err := Decode(buf.B, Options{})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-func BenchmarkDecodeSliceStructGob(b *testing.B) {
-	var buf bytes.Buffer
-
-	enc := gob.NewEncoder(&buf)
-	gob.Register(benchEncodeStruct{})
-
-	value := []benchEncodeStruct{
-		{
-			A: 3.14,
-			B: 3.15,
-			C: float64(3.18),
-		},
-		{
-			A: 3.15,
-			B: 3.14,
-			C: float64(3.19),
-		},
-	}
-	if err := enc.Encode(value); err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		var x []benchEncodeStruct
-		decbuf := bytes.NewBuffer(buf.Bytes())
-		dec := gob.NewDecoder(decbuf)
-		if err := dec.Decode(&x); err != nil {
 			b.Fatal(err)
 		}
 	}
