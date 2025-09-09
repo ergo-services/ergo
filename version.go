0508b83d8d1cb043f18f0af518167557ce90e041
Taras Halturin <halturin@gmail.com>
2025-09-09 22:22:32 +0200

    initial commit in 3.2.0

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ import "ergo.services/ergo/gen"
 var (
 	FrameworkVersion = gen.Version{
 		Name:    "Ergo Framework",
-		Release: "3.1.0",
+		Release: "3.2.0-development",
 		License: gen.LicenseMIT,
 	}
 )
